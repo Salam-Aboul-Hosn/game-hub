e1b7bbbb733871f22be5770ad8c5863592b84379
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-05-27 15:09:21 -0700

    displaying icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,10 +6,18 @@ interface FetchGamesResponse {
   count: number;
   results: Game[];
 }
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
