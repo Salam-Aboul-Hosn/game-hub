f461bc3ce276118e23bd5572ea67f02126a6bc8c
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-05-27 22:32:05 -0700

    Add emoji

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,7 +3,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageURL from "../services/image-url";
-
+import Emoji from "../components/Emoji";
 interface GameCardProps {
   game: Game;
 }
@@ -18,8 +18,10 @@ const GameCard = ({ game }: GameCardProps) => {
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
-          <Heading fontSize="2xl">{game.name}</Heading>
         </HStack>
+        <Heading fontSize="2xl">
+          {game.name} <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );
